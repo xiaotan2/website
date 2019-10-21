856c3e80c353803ab29c339441fa4351f856955f
Xiao Tan <xt85@cornell.edu>
2019-10-20 18:18:09 -0700

    Adding Website Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'learn';
+  showMenu = false;
+  darkModeActive: boolean;
+
+  userEmail = '';
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  ngOnDestroy() {}
+
 }
